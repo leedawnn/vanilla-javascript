8d91db0dd0c34db5e7ce47466ccaa38652378069
leedawnn <2g_hee@naver.com>
2023-03-02 23:46:15 +0900

    refactor: 이벤트 버블링

--- make-component/Modularity/components/Items.js
+++ make-component/Modularity/components/Items.js
@@ -8,16 +8,33 @@ export default class Items extends Component {
     const { items } = this.state;
     return `
       <ul>
-        ${items.map((item) => `<li>${item}</li>`).join('')}
+        ${items
+          .map(
+            (item, key) => `
+        <li>
+        ${item}
+        <button class="deleteBtn" data-index="${key}">삭제</button>
+        </li>`,
+          )
+          .join('')}
       </ul>
-      <button>추가</button>
+      <button class="addBtn">추가</button>
     `;
   }
 
   setEvent() {
-    this.$target.querySelector('button').addEventListener('click', () => {
-      const { items } = this.state;
-      this.setState({ items: [...items, `item${items.length + 1}`] });
+    // 이벤트 버블링을 사용하여 화면의 모든 요소에 일일이 이벤트 리스너를 추가하는 대신 컴포넌트의 this.$target에 이벤트 리스너를 달아놓고 하위에서 발생한 클릭 이벤트를 감지,,
+    this.$target.addEventListener('click', ({ target }) => {
+      const items = [...this.state.items];
+
+      if (target.classList.contains('addBtn')) {
+        this.setState({ items: [...items, `item${items.length + 1}`] });
+      }
+
+      if (target.classList.contains('deleteBtn')) {
+        items.splice(target.dataset.index, 1);
+        this.setState({ items });
+      }
     });
   }
 }
