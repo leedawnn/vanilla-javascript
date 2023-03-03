94c8b806a75c9aaaef58f7d65e157ee42d7f7287
leedawnn <2g_hee@naver.com>
2023-03-03 22:35:30 +0900

    refactor: 컴포넌트 분할

--- make-component/Modularity/src/core/Component.js
+++ make-component/Modularity/src/core/Component.js
@@ -1,27 +1,29 @@
 export default class Component {
   $target;
-  state;
-  constructor($target) {
+  $props;
+  $state;
+  constructor($target, $props) {
     this.$target = $target;
+    this.$props = $props;
     this.setup();
     this.setEvent(); // constructor에서 한 번만 실행된다.
     this.render();
   }
   setup() {}
+  mounted() {}
   template() {
     return '';
   }
   render() {
     this.$target.innerHTML = this.template();
+    this.mounted(); // render 후에 mounted가 실행된다.
   }
   setEvent() {}
   setState(newState) {
-    this.state = { ...this.state, ...newState };
+    this.$state = { ...this.$state, ...newState };
     this.render();
   }
-
   addEvent(eventType, selector, callback) {
-    const children = [...this.$target.querySelectorAll(selector)];
     this.$target.addEventListener(eventType, (event) => {
       if (!event.target.closest(selector)) return false;
       callback(event);
